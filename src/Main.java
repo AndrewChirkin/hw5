ba996eba2eab5fe8d334716b1d43e8bdef39a851
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-19 14:04:51 +0300

    hw5pt1ex4 for checking

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-    int [] numbers = new int[]{1, 2, 3};
-        System.out.print(numbers[2] + ", ");
-        System.out.print(numbers[1] + ", ");
-        System.out.println(numbers[0]);
-
-        double[] fractions = {1.57, 7.654, 9.986};
-        System.out.print(fractions[2] + ", ");
-        System.out.print(fractions[1] + ", ");
-        System.out.println(fractions[0]);
-
-   int [] fingers = new int[3];
-fingers[0] = 1;
-fingers[1] = 2;
-fingers[2] = 3;
-        System.out.print(fingers[2] + ", ");
-        System.out.print(fingers[1] + ", ");
-        System.out.println(fingers[0]);
+    int [] numbers = {1, 2, 3};
+    int firstNumber = numbers[0];
+    int secondNumber = numbers[1];
+    int thirdNumber = numbers[2];
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] %2 !=0){
+                numbers[i] = numbers[i] + 1;
+            }
+            System.out.println(numbers[i]);
+        }
     }
 }
