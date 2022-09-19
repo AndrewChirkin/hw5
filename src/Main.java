ad516b8c85a1987590d5a42dfddfd05d10066c23
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-19 10:16:41 +0300

    hw5pt1ex3 for checking

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
     int [] numbers = new int[]{1, 2, 3};
-        System.out.print(numbers[0] + ", ");
+        System.out.print(numbers[2] + ", ");
         System.out.print(numbers[1] + ", ");
-        System.out.println(numbers[2]);
+        System.out.println(numbers[0]);
 
         double[] fractions = {1.57, 7.654, 9.986};
-        System.out.print(fractions[0] + ", ");
+        System.out.print(fractions[2] + ", ");
         System.out.print(fractions[1] + ", ");
-        System.out.println(fractions[2]);
+        System.out.println(fractions[0]);
 
    int [] fingers = new int[3];
 fingers[0] = 1;
 fingers[1] = 2;
 fingers[2] = 3;
-        System.out.print(fingers[0] + ", ");
+        System.out.print(fingers[2] + ", ");
         System.out.print(fingers[1] + ", ");
-        System.out.println(fingers[2]);
+        System.out.println(fingers[0]);
     }
 }
