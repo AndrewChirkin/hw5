988cfffa4c4b2a4636360a2fabaf4c94b3ee0b8a
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-19 09:53:38 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    int [] numbers = new int[]{1, 2, 3};
+
+   double[] fractions = {1.57, 7.654, 9.986};
+
+   int [] fruits = new int[3];
+
+    }
+}
